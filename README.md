d6b2f101c47a2e84b30f028cff1d3bd742d9729f
michalkulisiewicz <michalkulisiewicz@gmail.com>
2022-08-25 22:28:13 +0200

    first commit

--- README.md
+++ README.md
@@ -1,6 +1,39 @@
 # woocommerce_baselinker_orders
-Script used to fetch orders from baselinker to woocommerce shop uses both baselinker and woocmommerce api.
-It checks every 15 minutes for new orders in specified status(default is: 'Dostaw Przez Sprzedającego')
-When there is an order in a status it's added to woocommers shop.
-After creating order status is changed for specifed (default is : 'Archiwum wysłanych')
-*In order to install all dependencies use command: pip install -r requirements.txt*
+<img src="misc/logo.png" width="400"/>
+
+Script used to fetch orders from baselinker to woocommerce shop, uses both baselinker and woocmommerce api.
+It checks every 15 minutes for new orders in specified status.
+If an order appears in a baselinker it's added to woocommers store.
+After creating order in woocommerce, baselinker status is changed for specifed by the user in config file.
+
+## Installation
+Use git clone to get the script:
+
+```shell
+git clone https://github.com/michalkulisiewicz/woocommerce_baselinker_orders.git
+```
+
+Install all requirements using pip:
+
+```shell
+pip install -r requirements.txt
+```
+## Configuration 
+In order to run the script you need to edit config.json file and 
+provide all necessary information
+
+## Running script
+Just run main.py file.
+
+## Contributing
+
+Bug reports and pull requests are welcome on GitHub at
+https://github.com/michalkulisiewicz/woocommerce_baselinker_orders. This project is intended to be a safe, welcoming space for collaboration, and contributors are expected to adhere to the [code of conduct](https://github.com/michalkulisiewicz/baselinker_subiekt_integrator/blob/master/CODE_OF_CONDUCT.md).
+
+## License
+
+Project is available as open source under the terms of the [MIT License](https://opensource.org/licenses/MIT).
+
+## Code of Conduct
+
+Everyone that interacts in the project codebase, issue trackers, chat rooms and mailing lists is expected to follow the [code of conduct](https://github.com/michalkulisiewicz/woocommerce_baselinker_orders/blob/master/CODE_OF_CONDUCT.md)
--- main.py
+++ main.py
@@ -5,40 +5,127 @@ from requests import Timeout
 import time
 import datetime
 import unidecode
+import os
 
 
-url = 'https://api.baselinker.com/connector.php'
-token = '1011-10210-OYC7U14WRGM51M3WD5OKJ9TZPSO60MXQ09MJL92BGZ71Z0ME9DERL4INTVCTT3DS'
+def read_config_file():
+    path = os.path.join(os.path.dirname(__file__), 'config.json')
+    with open(path, 'r') as f:
+        data = json.load(f)
+        return data
 
-wcapi = API(
-    url="https://alopl.nazwa.pl/wordpress/wpn_nowypress",
-    consumer_key="ck_4314dd53858243c893d4268ccbbd47f33a79b311",
-    consumer_secret="cs_033f1c30bc58846be334ebfeae444c302a8dd660",
-    wp_api=True,
-    version="wc/v3"
-)
+
+def set_up_woo_api(config_file):
+    wcapi = API(
+        url=config_file['woo_url'],
+        consumer_key=config_file['woo_consumer_key'],
+        consumer_secret=config_file['woo_consumer_secret'],
+        wp_api=True,
+        version=config_file['woo_version'],
+        timeout=120
+    )
+    return wcapi
+
+
+def search_woo_product_by_sku_update_price(sku, bs_price):
+    param = {'sku': sku}
+    res = wcapi.get("products", params=param).json()
+    if len(res) == 1:
+        woo_price = res[0]['price']
+        if bs_price != woo_price:
+            woo_type = res[0]['type']
+            woo_parent_id = res[0]['parent_id']
+            if woo_type == 'variation':
+                woo_variation_id = res[0]['id']
+                data = {
+                    "regular_price": bs_price
+                }
+                res = wcapi.put("products/{}/variations/{}".format(woo_parent_id, woo_variation_id), data).json()
+                if res['status'] == 400:
+                    return False
+                else:
+                    return True
+            else:
+                data = {
+                    "regular_price": bs_price
+                }
+                res = wcapi.put("products/{}".format(woo_parent_id), data).json()
+                if res['data']['status'] == 400:
+                    return False
+                else:
+                    return True
+
+        else:
+            print('bs_price is same as woo_price')
+            return True
+
+    else:
+        print('Product of sku:{} not found'.format(sku))
+        return False
+
+
+def search_woo_product_by_id(product_id, id, bs_price):
+    param = {'product_id': product_id}
+    res = wcapi.get("products", params=param).json()
+    if len(res) == 1:
+        woo_price = res[0]['price']
+        if bs_price != woo_price:
+            woo_type = res[0]['type']
+            woo_parent_id = res[0]['parent_id']
+            if woo_type == 'variation':
+                woo_variation_id = res[0]['id']
+                data = {
+                    "regular_price": bs_price
+                }
+                res = wcapi.put("products/{}/variations/{}".format(woo_parent_id, woo_variation_id), data).json()
+                if res['status'] == 400:
+                    return False
+                else:
+                    return True
+            else:
+                data = {
+                    "regular_price": bs_price
+                }
+                res = wcapi.put("products/{}".format(woo_parent_id), data).json()
+                if res['data']['status'] == 400:
+                    return False
+                else:
+                    return True
+
+        else:
+            print('bs_price is same as woo_price')
+            return True
+
+    else:
+        print('Product not found')
+        return False
+
 
 def del_hyphen(post_code):
-    post_code = post_code.replace('-','')
+    post_code = post_code.replace('-', '')
     return post_code
+
 
 def remove_accent(accented_string):
     unaccented_string = unidecode.unidecode(accented_string)
     return unaccented_string
 
+
 def get_first_name(fullname):
     first_name = fullname.split(' ')
     return first_name[0]
+
 
 def get_second_name(fullname):
     second_name = fullname.split(' ')
     return second_name[1]
 
+
 def get_orders_bs(source_status):
+    params = {'token': bs_token, 'method': 'getOrders',
+              'parameters': '{{ "storage_id": 1, "status_id": {} }}'.format(source_status)}
 
-    params = {'token': token, 'method': 'getOrders', 'parameters': '{{ "storage_id": 1, "status_id": {} }}'.format(source_status)}
-
-    response = requests.post(url, data=params)
+    response = requests.post(bs_url, data=params)
 
     response = response.text
 
@@ -46,45 +133,47 @@ def get_orders_bs(source_status):
 
     return orders_bs
 
-def set_order_status(order_id, status_id):
 
+def set_order_status(order_id, status_id):
     order_id = int(order_id)
     status_id = int(status_id)
 
-    params = {'token': token, 'method': 'setOrderStatus',
-              'parameters': '{"order_id": %i, "status_id": %i}' % (order_id,status_id)}
+    params = {'token': bs_token, 'method': 'setOrderStatus',
+              'parameters': '{"order_id": %i, "status_id": %i}' % (order_id, status_id)}
 
-    response = requests.post(url, data=params)
-
+    response = requests.post(bs_url, data=params)
 
     return response.status_code
 
 
+def delete_area_code(phone):
+    if '+48' in phone:
+        phone = phone.replace('+48', '')
+        return phone
+
+
 def parse_order(
-    payment_method,
-    invoice_fullname,
-    invoice_address,
-    invoice_city,
-    invoice_postcode,
-    invoice_country,
-    invoice_company,
-    email,
-    phone,
-    delivery_fullname,
-    delivery_address,
-    delivery_city,
-    delivery_postcode,
-    delivery_country,
-    delivery_company,
-    line_items_array,
-	shipping_lines_array,
-    currency,
-    shipping_total,
-    order_id
-    ):
-
-
-
+        payment_method,
+        invoice_fullname,
+        invoice_address,
+        invoice_city,
+        invoice_postcode,
+        invoice_country,
+        invoice_company,
+        email,
+        phone,
+        delivery_fullname,
+        delivery_address,
+        delivery_city,
+        delivery_postcode,
+        delivery_country,
+        delivery_company,
+        line_items_array,
+        shipping_lines_array,
+        currency,
+        shipping_total,
+        order_id
+):
     data = {}
     billing = {}
     shipping = {}
@@ -103,11 +192,10 @@ def parse_order(
     data['currency'] = currency
     data['shipping_total'] = shipping_total
 
-
     data['status'] = 'processing'
     data['set_paid'] = True
 
-    #check if invoice_fullname is not empty
+    # check if invoice_fullname is not empty
     if invoice_fullname:
         billing['first_name'] = get_first_name(invoice_fullname)
         billing['last_name'] = get_second_name(invoice_fullname)
@@ -123,6 +211,7 @@ def parse_order(
     billing['country'] = invoice_country
     billing['company'] = invoice_company
     billing['email'] = 'alopolskaali@gmail.com'
+    phone = delete_area_code(phone)
     billing['phone'] = phone
 
     if delivery_fullname:
@@ -131,7 +220,6 @@ def parse_order(
     else:
         shipping['first_name'] = ''
         shipping['last_name'] = ''
-        #print('Empty delivery address for order: {}')
 
     shipping['address_1'] = delivery_address
     shipping['address_2'] = ''
@@ -141,7 +229,7 @@ def parse_order(
     shipping['country'] = delivery_country
     shipping['company'] = delivery_company
 
-    #creates special fields in order
+    # creates special fields in order
     meta_data['key'] = 'baselinker_order_id'
     meta_data['value'] = order_id
 
@@ -149,7 +237,6 @@ def parse_order(
 
     return data
 
-    # TODO remember about default paramaters: email, sku, quantity, check if they are specified
 
 def create_woo_order(bs_orders):
     orders = []
@@ -180,7 +267,6 @@ def create_woo_order(bs_orders):
 
         order_id = str(id['order_id'])
 
-        line_items_array = []
         shipping_lines_array = []
 
         shipping_lines_dict = {}
@@ -189,47 +275,73 @@ def create_woo_order(bs_orders):
         shipping_lines_dict['total'] = str(id['delivery_price'])
         shipping_lines_array.append(shipping_lines_dict)
 
+        line_items_array = create_products_list(id, order_id)
+        if line_items_array:
+            orders.append(parse_order(payment_method,
+                                      invoice_fullname,
+                                      invoice_address,
+                                      invoice_city,
+                                      invoice_postcode,
+                                      invoice_country,
+                                      invoice_company,
+                                      email,
+                                      phone,
+                                      delivery_fullname,
+                                      delivery_address,
+                                      delivery_city,
+                                      delivery_postcode,
+                                      delivery_country,
+                                      delivery_company,
+                                      line_items_array,
+                                      shipping_lines_array,
+                                      currency,
+                                      shipping_total,
+                                      order_id))
+        else:
+            change_status = set_order_status(order_id, '86800')
+            if change_status != 200:
+                print('Issue with changing status for order_id: {}'.format(order_id))
+            else:
+                print('Changed failed order status order id: {} to {} at: {}'.format(order_id, dest_status,
+                                                                                     datetime.datetime.now()))
+        return orders
 
-        for product in id['products']:
-            line_items_dict = {}
-            #line_items_dict['sku'] = product['sku']
-            line_items_dict['sku'] = product['sku']
-            line_items_dict['quantity'] = product['quantity']
-            line_items_dict['price'] = str(product['price_brutto'])
+
+def create_products_list(id, order_id):
+    line_items_array = []
+
+    for product in id['products']:
+        line_items_dict = {}
+        bs_price = str(product['price_brutto'])
+        sku = product['sku']
+        line_items_dict['sku'] = sku
+        line_items_dict['quantity'] = product['quantity']
+        line_items_dict['price'] = bs_price
+
+        correct_price = search_woo_product_by_sku_update_price(sku, bs_price)
+        if correct_price == True:
             line_items_array.append(line_items_dict)
+        else:
+            print('Issue with order_id:{} with sku:{}'.format(order_id, sku))
+            return None
+
+    return line_items_array
 
 
-        orders.append(parse_order(payment_method,
-                                  invoice_fullname,
-                                  invoice_address,
-                                  invoice_city,
-                                  invoice_postcode,
-                                  invoice_country,
-                                  invoice_company,
-                                  email,
-                                  phone,
-                                  delivery_fullname,
-                                  delivery_address,
-                                  delivery_city,
-                                  delivery_postcode,
-                                  delivery_country,
-                                  delivery_company,
-                                  line_items_array,
-								  shipping_lines_array,
-                                  currency,
-                                  shipping_total,
-                                  order_id))
-    return orders
+def check_available_orders():
+    try:
+        bs_orders = get_orders_bs(source_status)
+    except:
+        time.sleep(30 * 60)
+        bs_orders = get_orders_bs(source_status)
 
-
-def chk_available_orders():
-    bs_orders = get_orders_bs(source_status)
     if not bs_orders['orders']:
         print('No available orders in status: {} checked at: {}'.format(source_status, datetime.datetime.now()))
-        time.sleep(30*60)
-        return chk_available_orders()
+        time.sleep(30 * 60)
+        return check_available_orders()
     else:
         return bs_orders
+
 
 def get_bs_order_id(order):
     meta_data_list = order['meta_data']
@@ -237,6 +349,7 @@ def get_bs_order_id(order):
         if item['key'] == 'baselinker_order_id':
             order_id = item['value']
             return order_id
+
 
 def check_if_order_exist(new_order_id):
     orders = wcapi.get("orders").json()
@@ -248,63 +361,61 @@ def check_if_order_exist(new_order_id):
             return False
 
 
-source_status = input('Provide source status [press enter for default]: ')
+def run():
+    config_file = read_config_file()
+    global bs_url, bs_token, wcapi, source_status, dest_status
+    bs_url = config_file['bs_api_endpoint']
+    bs_token = config_file['bs_token']
+    wcapi = set_up_woo_api(config_file)
+    source_status = config_file['bs_source_status']
+    dest_status = config_file['woo_destination_status']
 
-if source_status == '':
-    source_status = 153957
+    while True:
+        bs_orders = check_available_orders()
 
-dest_status = input('Provide destination status: [press enter for default]: ')
+        orders = create_woo_order(bs_orders)
 
-if dest_status == '':
-    dest_status = 166337
-
-
-while True:
-
-
-    bs_orders = chk_available_orders()
-
-    orders = create_woo_order(bs_orders)
-
-
-    for order in orders:
-        try:
-            resp = wcapi.post("orders", order)
-        except Timeout as err:
-            print('Request timed out at: {} with error: {}'.format(datetime.datetime.now(), err))
-            new_order_id = get_bs_order_id(order)
-            order_exist = check_if_order_exist(new_order_id)
-            if order_exist == True:
-                print('Order {} timeout but was sucessfully added'.format(new_order_id))
-                change_status = set_order_status(new_order_id, dest_status)
-                if change_status != 200:
-                    print('Issue with changing status for order_id: {}'.format(new_order_id))
-                else:
-                    print('Changed status of order id: {} to {} at: {}'.format(new_order_id, dest_status,
-                                                                               datetime.datetime.now()))
-
-        if resp.status_code == 201:
-            resp_json = resp.json()
-
-            for id in resp_json['meta_data']:
-                if id['key'] == 'baselinker_order_id':
-                    order_id = id['value']
-                    change_status = set_order_status(order_id, dest_status)
+        for order in orders:
+            try:
+                resp = wcapi.post("orders", order)
+            except Timeout as err:
+                print('Request timed out at: {} with error: {}'.format(datetime.datetime.now(), err))
+                new_order_id = get_bs_order_id(order)
+                order_exist = check_if_order_exist(new_order_id)
+                if order_exist == True:
+                    print('Order {} timeout but was sucessfully added'.format(new_order_id))
+                    change_status = set_order_status(new_order_id, dest_status)
                     if change_status != 200:
-                        print('Issue with changing status for order_id: {}'.format(order_id))
+                        print('Issue with changing status for order_id: {}'.format(new_order_id))
                     else:
-                        print('Changed status of order id: {} to {} at: {}'.format(order_id, dest_status, datetime.datetime.now() ) )
+                        print('Changed status of order id: {} to {} at: {}'.format(new_order_id, dest_status,
+                                                                                   datetime.datetime.now()))
 
-        elif resp.status_code == 400:
-            resp_json = resp.json()
-            for id in order['meta_data']:
-                if id['key'] == 'baselinker_order_id':
-                    order_id = id['value']
-                    message = resp_json['message']
-                    print('Issues with order_id: {}. {}'.format(order_id, message))
-        else:
-            for id in order['meta_data']:
-                if id['key'] == 'baselinker_order_id':
-                    order_id = id['value']
-                    print('Issues with order_id: {}'.format(order_id))
+            if resp.status_code == 201:
+                resp_json = resp.json()
 
+                for id in resp_json['meta_data']:
+                    if id['key'] == 'baselinker_order_id':
+                        order_id = id['value']
+                        change_status = set_order_status(order_id, dest_status)
+                        if change_status != 200:
+                            print('Issue with changing status for order_id: {}'.format(order_id))
+                        else:
+                            print('Changed status of order id: {} to {} at: {}'.format(order_id, dest_status,
+                                                                                       datetime.datetime.now()))
+
+            elif resp.status_code == 400:
+                resp_json = resp.json()
+                for id in order['meta_data']:
+                    if id['key'] == 'baselinker_order_id':
+                        order_id = id['value']
+                        message = resp_json['message']
+                        print('Issues with order_id: {}. {}'.format(order_id, message))
+            else:
+                for id in order['meta_data']:
+                    if id['key'] == 'baselinker_order_id':
+                        order_id = id['value']
+                        print('Issues with order_id: {}'.format(order_id))
+
+
+run()
